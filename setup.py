d6b6f8d11bf92957f67da459ec18f3200248096e
Thomas <@7lieues.io>
2020-05-28 16:33:47 +0200

    0.11

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+import codecs
+import os
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+HERE = os.path.abspath(os.path.dirname(__file__))
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
 setup(
   name = 'vaderSentiment_fr',         # How you named your package folder (MyLib)
-  packages = ['vaderSentiment_fr'],   # Chose the same as "name"
+  packages = find_packages(),   # Chose the same as "name"
   include_package_data=True,
-  version = '0.10',      # Start with a small number and increase it with every change you make
+  version = '0.11',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'French version of VADER',   # Give a short description about your library
+  long_description = read("README.rst"),
+  long_description_content_type = 'text/x-rst',   # Give a short description about your library
   author = 'DEPREZ Olivier, EK Thomas',                   # Type in your name
   author_email = 'olivier@7lieues.io, thomas@7lieues.io',      # Type in your E-Mail
   url = 'https://github.com/thomas7lieues/vader_FR',   # Provide either the link to your github or to your website
