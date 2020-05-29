c02f8ef7eb61b96d09efd31eeba22b02c7411eba
Thomas <@7lieues.io>
2020-05-29 10:16:30 +0200

    Correcting find_word problem

--- vaderSentiment_fr/tree.py
+++ vaderSentiment_fr/tree.py
@@ -70,3 +70,36 @@ def find_prefix(root, prefix: str) -> Tuple[bool, int]:
     # And also the counter of the last node. This indicates how many words have this
     # prefix
     return True, node.counter
+
+def find_word(root, prefix: str) -> Tuple[bool, int]:
+    """
+    Check and return
+      1. If the prefix exsists in any of the words we added so far
+      2. If yes then how may words actually have the prefix
+    """
+    node = root
+    # If the root node has no children, then return False.
+    # Because it means we are trying to search in an empty trie
+    if not root.children:
+        return False
+    n = len(prefix)
+    for char in prefix:
+        char_not_found = True
+        # Search through all the children of the present `node`
+        for child in node.children:
+            if child.char == char:
+                # We found the char existing in the child.
+                char_not_found = False
+                # Assign node as the child containing the char and break
+                node = child
+                break
+        # Return False anyway when we did not find a char.
+        if char_not_found:
+            return False
+    # Well, we are here means we have found the prefix. Return true to indicate that
+    # And also the counter of the last node. This indicates how many words have this
+    # prefix
+    if node.word_finished == True:
+        return True
+
+    return False
